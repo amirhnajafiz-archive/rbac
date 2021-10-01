4bc6a043a5d7c854cd6942fb92ec81eb8f3a099c
9831065 <najafizadeh21@gmail.com>
2021-10-01 15:24:59 +0330

    Database seeder added

--- power-gates/app/Models/User.php
+++ power-gates/app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -41,4 +42,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Setting user password.
+     *
+     * @param $value
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
 }
--- power-gates/database/seeders/DatabaseSeeder.php
+++ power-gates/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Super admin
+        User::query()->create([
+            'name' => 'Super admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => '1234',
+            'remember_token' => Str::random(10),
+        ]);
+
+        // Users seeding
+        User::factory(10)->create();
     }
 }
